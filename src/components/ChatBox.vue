4de1c0e91c4f6b1b1db2cffd15f8441af13d60f0
Cam895 <113192788+Cam895@users.noreply.github.com>
2022-10-17 14:18:53 +0200

    Update ChatBox.vue
    MAJ Bot

--- src/components/ChatBox.vue
+++ src/components/ChatBox.vue
@@ -5,22 +5,27 @@
 </template>
 
 
-<script>
-export default {
-    method: {
-
-    },
-    mounted() {
+<script type="text/javascript">
     (function(d, m){
         var kommunicateSettings = 
-            {"appId":"33fdbc7d8cb8fc574b7d8e16c5de0a210","popupWidget":true,"automaticChatOpenOnNavigation":true};
+            {"appId":"1247afd6211c42f93eb177d3ce256014c",
+            "popupWidget":false,
+            "automaticChatOpenOnNavigation":false, 
+            "hidePostCTA":true, 
+            "oneTimeRating": true, 
+            "timeFormat24Hours": true,
+
+            "onInit": function (){                
+            window.KommunicateGlobal.document.querySelector(".mck-box-form.mck-row").style.visibility="hidden";
+            window.KommunicateGlobal.querySelector(".mck-box-form.mck-row").style.display="none";
+            }
+          };
+            kommunicateSettings.restartConversationByUser = true;
         var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
         s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
-        var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+        var h = document.getElementsByTagName("head")[0]; h.appendChild(s)
         window.kommunicate = m; m._globals = kommunicateSettings;
     })(document, window.kommunicate || {});
-    }
-}
 </script>
 
 
